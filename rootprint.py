7a3f4814ac4cec7996c5032d0d292760743fde56
Christian Gumpert <cgumpert@cern.ch>
2015-04-13 16:39:59 +0200

    make rootprint available as cell magic

--- rootprint.py
+++ rootprint.py
@@ -4,30 +4,34 @@
 # It must be the first element in the cell.
 import tempfile
 import ROOT
-from IPython.core.magic import (Magics, magics_class, cell_magic)
+from IPython.core.magic import (Magics, magics_class, line_cell_magic)
 
 @magics_class
 class RootMagics(Magics):
     """Magics related to Root.
 
-    %%rootprint  - Capture Root stdout output and show in result cell
+    %%rp  - Capture Root stdout output and show in result cell
     """
 
     def __init__(self, shell):
         super(RootMagics, self).__init__(shell)
 
-    @cell_magic
-    def rootprint(self, line, cell):
+    @line_cell_magic
+    def rp(self, line, cell=None):
         """Capture Root stdout output and print in ipython notebook."""
 
         with tempfile.NamedTemporaryFile() as tmpFile:
-
             ROOT.gSystem.RedirectOutput(tmpFile.name, "w")
             # ns = {}
             # exec cell in self.shell.user_ns, ns
-            exec cell in self.shell.user_ns
-            ROOT.gROOT.ProcessLine("gSystem->RedirectOutput(0);")
-            print tmpFile.read()
+            if cell:
+                exec cell in self.shell.user_ns
+                ROOT.gROOT.ProcessLine("gSystem->RedirectOutput(0);")
+                print tmpFile.read()
+            else:
+                exec line in self.shell.user_ns
+                ROOT.gROOT.ProcessLine("gSystem->RedirectOutput(0);")
+                print tmpFile.read()
 
 # Register
 ip = get_ipython() 
